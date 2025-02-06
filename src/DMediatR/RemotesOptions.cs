a53e3f7ad6dfdad53baea85854bd26b697fa0aaa
toniarnold <toni__arnold@bluewin.ch>
2025-02-06 08:42:29 +0100

    Disable false positive SYSLIB1212/3 warnings

--- src/DMediatR/RemotesOptions.cs
+++ src/DMediatR/RemotesOptions.cs
@@ -6,6 +6,11 @@ namespace DMediatR
     internal partial class ValidateRemotesOptions : IValidateOptions<RemotesOptions>
     { }
 
+    // The [OptionsValidator] actually validates [ValidateEnumeratedItems], thus ignore all related warnings:
+#pragma warning disable IDE0079
+#pragma warning disable SYSLIB1212
+#pragma warning disable SYSLIB1213
+
     /// <summary>
     /// Dictionary of HostOptions declaring the configured remotes for that DMediatR node.
     /// </summary>
@@ -16,4 +21,8 @@ namespace DMediatR
         [ValidateEnumeratedItems]
         public IEnumerable<HostOptions> ValidatedValues => Values;
     }
+
+#pragma warning restore SYSLIB1213
+#pragma warning restore SYSLIB1212
+#pragma warning restore IDE0079
 }
